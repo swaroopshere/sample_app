3d6c4ce942b8e9af4d94be5d1c74e02d4b27482b
Swaroop Shere <sshere@localhost.(none)>
2012-01-19 11:32:52 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4758a7dc15a5ab93e5136d59d1d7eb91fe9384286474e6ac06a376f460e7edbf008f472f61ae799fdf63d4ae122042b69a1d92b0cdedea1d3388aa80ca00262f'
